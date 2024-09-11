941c6ccff192f19e4307c801a1595d88517c770a
jay <jay@grolar>
2024-09-11 21:34:47 +0200

    mv ls into gpx receiver

--- cmd/ls.go
+++ cmd/ls.go
@@ -1,9 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-	"slices"
-
 	. "github.com/jple/gpx-cli/core"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -18,11 +15,10 @@ func CreateLsCmd() *cobra.Command {
 		Run: func(cmd *cobra.Command, args []string) {
 
 			// ascii_format, _ := strconv.ParseBool(args[0])
-			trkNames := ls(viper.Get("filename").(string), viper.GetBool("all"))
-			trkNames.Print(viper.GetBool("all"))
-			// for i, trackName := range ls(viper.Get("filename").(string)) {
-			// 	fmt.Printf("%v: \u001b[1;32m%v\u001b[22;0m\n", i, trackName)
-			// }
+			// trkNames := ls(viper.GetString("filename"), viper.GetBool("all"))
+			Gpx{Filepath: viper.GetString("filename")}.
+				Ls(viper.GetBool("all")).
+				Print(viper.GetBool("all"))
 		},
 	}
 
@@ -34,48 +30,4 @@ func CreateLsCmd() *cobra.Command {
 	})
 
 	return cmd
-}
-
-type TrkName struct {
-	TrkName    string
-	TrkptNames []string
-}
-type TrkNames []TrkName
-
-func ls(gpxFilename string, all bool) TrkNames {
-	var gpx Gpx
-	gpx.ParseFile(gpxFilename)
-
-	var out TrkNames
-	for i, trk := range gpx.Trk {
-		out = append(out, TrkName{TrkName: trk.Name})
-
-		if all {
-			trkpts := slices.Concat(trk.Trkseg)[0].Trkpt
-			for _, trkpt := range trkpts {
-				if trkpt.Name != nil {
-					out[i].TrkptNames = append(out[i].TrkptNames, *trkpt.Name)
-				}
-			}
-		}
-	}
-
-	return out
-}
-
-func (trkNames TrkNames) Print(all bool, ascii_format ...bool) {
-
-	for i, trkName := range trkNames {
-		if len(ascii_format) > 0 && !ascii_format[0] {
-			fmt.Printf("[%v] %v\n", i, trkName.TrkName)
-		} else {
-			fmt.Printf("[%v] \u001b[1;32m%v\u001b[22;0m\n", i, trkName.TrkName)
-		}
-		if all {
-			for _, trkptName := range trkName.TrkptNames {
-				fmt.Println(trkptName)
-			}
-			fmt.Println()
-		}
-	}
 }
--- core/gpx.go
+++ core/gpx.go
@@ -4,6 +4,7 @@ import (
 	"encoding/xml"
 	"fmt"
 	"os"
+	"slices"
 )
 
 func (gpx *Gpx) ParseFile(gpxFilename string) {
@@ -17,12 +18,13 @@ func (gpx *Gpx) SetVitesse(v float64) {
 	(*gpx).Extensions.Vitesse = v
 }
 
-func (gpx *Gpx) Info(gpxFilename string, vitessePlat float64, detail bool, ascii_format bool) {
-	(*gpx).ParseFile(gpxFilename)
-	(*gpx).SetVitesse(vitessePlat)
+func (p_gpx *Gpx) Info(vitessePlat float64, detail bool, ascii_format bool) {
+	gpx := *p_gpx
+	gpx.ParseFile(gpx.Filepath)
+	gpx.SetVitesse(vitessePlat)
 
 	for i, trk := range gpx.Trk {
-		summary := trk.CalcAll((*gpx).Extensions.Vitesse, detail)
+		summary := trk.CalcAll(gpx.Extensions.Vitesse, detail)
 		for _, s := range summary {
 
 			fmt.Printf("[%v] ", i)
@@ -33,4 +35,47 @@ func (gpx *Gpx) Info(gpxFilename string, vitessePlat float64, detail bool, ascii
 		fmt.Println()
 	}
 
+}
+
+type TrkName struct {
+	TrkName    string
+	TrkptNames []string
+}
+type TrkNames []TrkName
+
+func (gpx Gpx) Ls(all bool) TrkNames {
+	gpx.ParseFile(gpx.Filepath)
+
+	var out TrkNames
+	for i, trk := range gpx.Trk {
+		out = append(out, TrkName{TrkName: trk.Name})
+
+		if all {
+			trkpts := slices.Concat(trk.Trkseg)[0].Trkpt
+			for _, trkpt := range trkpts {
+				if trkpt.Name != nil {
+					out[i].TrkptNames = append(out[i].TrkptNames, *trkpt.Name)
+				}
+			}
+		}
+	}
+
+	return out
+}
+
+func (trkNames TrkNames) Print(all bool, ascii_format ...bool) {
+
+	for i, trkName := range trkNames {
+		if len(ascii_format) > 0 && !ascii_format[0] {
+			fmt.Printf("[%v] %v\n", i, trkName.TrkName)
+		} else {
+			fmt.Printf("[%v] \u001b[1;32m%v\u001b[22;0m\n", i, trkName.TrkName)
+		}
+		if all {
+			for _, trkptName := range trkName.TrkptNames {
+				fmt.Println(trkptName)
+			}
+			fmt.Println()
+		}
+	}
 }
