e58aeb4836f486485af7e8f1552609d23a8c1d88
jay <jay@grolar>
2025-07-27 07:10:56 +0200

    chore: rename dist to calc-dist

--- cmd/calc_dist.go
+++ cmd/calc_dist.go
@@ -8,9 +8,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func CreateDistCmd() *cobra.Command {
+func CreateCalcDistCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "dist [lat1] [lon1] [lat2] [lon2]",
+		Use:   "calc-dist [lat1] [lon1] [lat2] [lon2]",
 		Short: "Calculate distance (km) based on GPS coordinates",
 		Long: `Calculate distance (km) based on GPS coordinates
 Expects 4 float positional arguments (lat1, lon1, lat2, lon2)
