2206acec4eb9f3d1a25bd7bb48ce1159b58902ab
jay <jay@grolar>
2025-08-01 20:59:21 +0200

    add dist.go

---
+++ cmd/dist.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	. "github.com/jple/gpx-cli/core"
+)
+
+func CreateCalcInfoCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "dist",
+		Short: "Calculate info (km, elev, km_e, duration) between two named trkpt",
+		Run: func(cmd *cobra.Command, args []string) {
+			gpx := Gpx{}
+			gpx.ParseFile(viper.GetString("filename"))
+			gpx.SetVitesse(4.5)
+
+			// TODO: WIP
+
+		},
+	}
+
+	return cmd
+}
